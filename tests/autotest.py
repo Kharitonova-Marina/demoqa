28fdd1d2f12a669fbc7c3fce51904cd3370f73f4
Maria Haritonova <mariaharitonova@MacBook-Air-Admin.local>
2023-03-06 14:31:28 +0300

    added date picker

--- tests/autotest.py
+++ tests/autotest.py
@@ -1,5 +1,4 @@
 import os
-from datetime import date
 
 from selene import browser, command, have
 from selene.support.conditions.be import not_
@@ -12,7 +11,7 @@ def test_fill_and_send_form(preparations):
     browser.execute_script('document.querySelector("footer").remove()')
     browser.execute_script('document.querySelector("#fixedban").remove()')
     browser.execute_script('document.querySelector("#RightSide_Advertisement").remove()')
-    #endregion
+    # endregion
 
     # region FILL FORM
     browser.element('#firstName').type('Marina')
@@ -20,9 +19,17 @@ def test_fill_and_send_form(preparations):
     browser.element('#userEmail').type('tests@testmail.com')
     browser.element("[for='gender-radio-2']").click()
     browser.element('#userNumber').type('9112223344')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('.react-datepicker__month-select > option[value="2"]').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('.react-datepicker__year-select > option[value="1991"]').click()
+    browser.element('div[aria-label*="March 7th"]').click()
+
     browser.element('#subjectsInput').type('ma').press_enter()
     browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('#uploadPicture').send_keys(os.path.normpath(os.getcwd() + os.sep + os.pardir) + '/source/picture.jpeg')
+    browser.element('#uploadPicture').send_keys(
+        os.path.normpath(os.getcwd() + os.sep + os.pardir) + '/resources/picture.jpeg')
     browser.element('#currentAddress').type('Russia')
 
     browser.element('#state').click()
@@ -40,7 +47,7 @@ def test_fill_and_send_form(preparations):
     browser.element('//table/tbody/tr[2]/td[2]').should(have.text('tests@testmail.com'))
     browser.element('//table/tbody/tr[3]/td[2]').should(have.text('Female'))
     browser.element('//table/tbody/tr[4]/td[2]').should(have.text('9112223344'))
-    browser.element('//table/tbody/tr[5]/td[2]').should(have.text((date.today().strftime("%d %B,%Y"))))
+    browser.element('//table/tbody/tr[5]/td[2]').should(have.text('07 March,1991'))
     browser.element('//table/tbody/tr[6]/td[2]').should(have.text('Maths'))
     browser.element('//table/tbody/tr[7]/td[2]').should(have.text('Sports'))
     browser.element('//table/tbody/tr[8]/td[2]').should(have.text('picture.jpeg'))
