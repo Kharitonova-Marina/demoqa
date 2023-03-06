15d201a02a261538147141e765db21c65840c99b
Maria Haritonova <mariaharitonova@MacBook-Air-Admin.local>
2023-03-06 11:15:55 +0300

    added deleting ad

--- conftest.py
+++ conftest.py
@@ -5,5 +5,5 @@ from selene.support.shared import browser
 @pytest.fixture(scope="session")
 def preparations():
     browser.config.hold_browser_open = False
-    browser.config.window_width = 1024
-    browser.config.window_height = 768
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
--- tests/autotest.py
+++ tests/autotest.py
@@ -8,9 +8,13 @@ from selene.support.conditions.be import not_
 def test_fill_and_send_form(preparations):
     browser.open('https://demoqa.com/automation-practice-form')
 
-    # region FILL FORM
+    # region DELETE ADVERTISEMENT
     browser.execute_script('document.querySelector("footer").remove()')
+    browser.execute_script('document.querySelector("#fixedban").remove()')
+    browser.execute_script('document.querySelector("#RightSide_Advertisement").remove()')
+    #endregion
 
+    # region FILL FORM
     browser.element('#firstName').type('Marina')
     browser.element('#lastName').type('Kharitonova')
     browser.element('#userEmail').type('tests@testmail.com')
