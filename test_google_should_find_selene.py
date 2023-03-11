29acc37e32fb386354a522d6c8f06d6555f69573
Maria Haritonova <mariaharitonova@MacBook-Air-Admin.local>
2023-03-11 17:09:12 +0300

    fix

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,12 +1,13 @@
 from selene.support.shared import browser
 from selene import be, have
 
-
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
-
 def test_searching(preparations):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_searching_empty(preparations):
     browser.open('https://www.google.com')
     browser.element('[name="q"]').should(be.blank).type('артольрапвчрплпанверкпиамьпаоеикпвыввфсфыфычфыч').press_enter()
     browser.element('[id="center_col"]').should(have.text('По запросу артольрапвчрплпанверкпиамьпаоеикпвыввфсфыфычфыч ничего не найдено.'))
